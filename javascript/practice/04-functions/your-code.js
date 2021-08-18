48589353255d07b05214241e2c178c0f32396cb5
Max Manayan <maximillian521@gmail.com>
2021-08-18 12:50:24 -0600

    JS 04-functions complete

--- javascript/practice/04-functions/your-code.js
+++ javascript/practice/04-functions/your-code.js
@@ -4,11 +4,12 @@
 
 // 1. ADD TWO NUMBERS
 // ==================
-// Complete the following function. 
+// Complete the following function.
 // Write code to add firstNumber and secondNumber and then return the result.
 
 function addTwoNumbers(firstNumber, secondNumber) {
-    // your code goes here
+  // your code goes here
+  return firstNumber + secondNumber;
 }
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
@@ -23,7 +24,7 @@ console.log("112 + 0 -> expected: 112, actual: %s", result);
 result = addTwoNumbers(-53, 40);
 console.log("-53 + 40 -> expected: -13, actual: %s", result);
 
-// 2. FIRST LETTER IN A PHRASE 
+// 2. FIRST LETTER IN A PHRASE
 //   (first character in a string)
 // =============================
 // Complete the following function.
@@ -31,14 +32,27 @@ console.log("-53 + 40 -> expected: -13, actual: %s", result);
 // Write code to return the first character in text.
 // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String#Character_access
 function getFirstCharacter(text) {
-    // your code goes here.
+  // your code goes here.
+  return text.charAt(0);
 }
 
 console.log("getFirstCharacter =====");
-console.log("'strawberry' -> expected: s, actual: %s.", getFirstCharacter("strawberry"));
-console.log("'above the sea' -> expected: a, actual: %s.", getFirstCharacter("above the sea"));
-console.log("'Starling' -> expected: S, actual: %s.", getFirstCharacter("Starling"));
-console.log("'Olympus Mons' -> expected: O, actual: %s.", getFirstCharacter("Olympus Mons"));
+console.log(
+  "'strawberry' -> expected: s, actual: %s.",
+  getFirstCharacter("strawberry")
+);
+console.log(
+  "'above the sea' -> expected: a, actual: %s.",
+  getFirstCharacter("above the sea")
+);
+console.log(
+  "'Starling' -> expected: S, actual: %s.",
+  getFirstCharacter("Starling")
+);
+console.log(
+  "'Olympus Mons' -> expected: O, actual: %s.",
+  getFirstCharacter("Olympus Mons")
+);
 
 // 3. MAX
 // =======
@@ -48,17 +62,26 @@ console.log("'Olympus Mons' -> expected: O, actual: %s.", getFirstCharacter("Oly
 // the correct result.
 // Write your code here:
 
+function max(n1, n2, n3) {
+  if (n1 >= n2 && n1 >= n3) return n1;
+
+  if (n2 >= n1 && n2 >= n3) return n2;
+
+  if (n3 >= n1 && n3 >= n2) return n3;
+}
+
 // Uncomment the code below to verify your function is working.
-// console.log("max =====");
-// console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
-// console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
-// console.log("max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s", max(-15.67, 33.293, 0.51));
-// console.log("max(4, 987654321, 0) -> expected: 987654321, actual: %s", max(4, 987654321, 0));
-
-
-
-
-
+console.log("max =====");
+console.log("max(-1, 0, 1) -> expected: 1, actual: %s", max(-1, 0, 1));
+console.log("max(11, 2, 1) -> expected: 11, actual: %s", max(11, 2, 1));
+console.log(
+  "max(-15.67, 33.293, 0.51) -> expected: 33.293, actual: %s",
+  max(-15.67, 33.293, 0.51)
+);
+console.log(
+  "max(4, 987654321, 0) -> expected: 987654321, actual: %s",
+  max(4, 987654321, 0)
+);
 
 /**********************************
  * The Software Guild
